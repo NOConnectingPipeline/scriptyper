9badfd962a521b2749d2827a4dc2ed4ee609caa3
AYATSUKO2 <x10cd402PRaya0314@gmail.com>
2022-03-08 09:52:24 +0900

    機能復活＆追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,91 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import TypingGameHtml from './components/TypingGameHtml';
+import TypingGameCss from './components/TypingGameCss';
+import TypingGameJavascript from './components/TypingGameJavascript';
+import TypingGameGit from './components/TypingGameGit';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+  const modeArray = ['html', 'css', 'js', 'git', 'RESET'];
+  const [mode, setMode] = useState(modeArray[4])
+  switch(mode) {
+    case modeArray[0]:
+      return (
+        <div className="App">
+        <header className="App-header">
+          <ul className="App-modenav">
+            <li className='App-modenav-li' onClick = {() => {setMode(mode[0])}}>HTML</li>
+            <li className='App-modenav-li' onClick = {() => {setMode(mode[1])}}>CSS</li>
+            <li className='App-modenav-li' onClick = {() => {setMode(mode[2])}}>Javascript</li>
+            <li className='App-modenav-li' onClick = {() => {setMode(mode[3])}}>Git</li>  
+          </ul>
+          <img src={logo} className="App-logo" alt="logo" />
+          <TypingGameHtml />
+        </header>
+      </div>
+      );
+    case modeArray[1]:
+      return (
+        <div className="App">
+        <header className="App-header">
+          <ul className="App-modenav">
+            <li className='App-modenav-li' onClick = {() => {setMode(modeArray[0])}}>HTML</li>
+            <li className='App-modenav-li' onClick = {() => {setMode(modeArray[1])}}>CSS</li>
+            <li className='App-modenav-li' onClick = {() => {setMode(modeArray[2])}}>Javascript</li>
+            <li className='App-modenav-li' onClick = {() => {setMode(modeArray[3])}}>Git</li>  
+          </ul>
+          <img src={logo} className="App-logo" alt="logo" />
+          <TypingGameCss />
+        </header>
+      </div>
+      );
+    case modeArray[2]:
+      return (
+        <div className="App">
+        <header className="App-header">
+          <ul className="App-modenav">
+            <li className='App-modenav-li' onClick = {() => {setMode(modeArray[0])}}>HTML</li>
+            <li className='App-modenav-li' onClick = {() => {setMode(modeArray[1])}}>CSS</li>
+            <li className='App-modenav-li' onClick = {() => {setMode(modeArray[2])}}>Javascript</li>
+            <li className='App-modenav-li' onClick = {() => {setMode(modeArray[3])}}>Git</li>  
+          </ul>
+          <img src={logo} className="App-logo" alt="logo" />
+          <TypingGameJavascript />
+        </header>
+      </div>
+      );
+    case modeArray[3]:
+      return (
+        <div className="App">
+        <header className="App-header">
+          <ul className="App-modenav">
+            <li className='App-modenav-li' onClick = {() => {setMode(modeArray[0])}}>HTML</li>
+            <li className='App-modenav-li' onClick = {() => {setMode(modeArray[1])}}>CSS</li>
+            <li className='App-modenav-li' onClick = {() => {setMode(modeArray[2])}}>Javascript</li>
+            <li className='App-modenav-li' onClick = {() => {setMode(modeArray[3])}}>Git</li>  
+          </ul>
+          <img src={logo} className="App-logo" alt="logo" />
+          <TypingGameGit />
+        </header>
+      </div>
+      );
+    case modeArray[4]:
+      return (
+        <div className="App">
+        <header className="App-header">
+          <ul className="App-modenav">
+            <li className='App-modenav-li' onClick = {() => {setMode(modeArray[0])}}>HTML</li>
+            <li className='App-modenav-li' onClick = {() => {setMode(modeArray[1])}}>CSS</li>
+            <li className='App-modenav-li' onClick = {() => {setMode(modeArray[2])}}>Javascript</li>
+            <li className='App-modenav-li' onClick = {() => {setMode(modeArray[3])}}>Git</li>  
+          </ul>
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>モードを選択してください。</p>
+        </header>
+      </div>
+      );
+  }
 }
 
 export default App;
---
+++ src/components/TypingGameCss.tsx
@@ -0,0 +1,173 @@
+// import it
+import React,{useState, useEffect} from "react";
+import WordContainment from "../wordContainment.json";
+import styles from "./TypingGameComponent.module.scss";
+
+const randomWordCss: string[] = [];
+const randomMeanCss: string[] = [];
+const randomLevelCss: number[] = [];
+
+const max = 10
+
+const lengthJSONCss: number = WordContainment.css.length;
+
+function numRandom(max: number) {
+  return Math.floor(Math.random() * max);
+}
+
+for(let i = 0; i < max; i++){
+  while(true) {
+    let tmp = numRandom(lengthJSONCss)
+    if(!randomWordCss.includes(WordContainment.css[tmp].word)){
+      randomWordCss.push(WordContainment.css[tmp].word);
+      randomMeanCss.push(WordContainment.css[tmp].mean);
+      randomLevelCss.push(WordContainment.css[tmp].level);
+      break;
+    }
+  }
+}
+
+let result: number = 0;
+
+// type APIWord = {
+//   word: string[],
+//   mean: string[],
+//   level: number[],
+// };
+
+//exporting function
+const TypingGameCss = () => {
+
+  // const [array, setArray] = useState({
+  //   word: [],
+  //   mean: [],
+  //   level: [],
+  // } as APIWord)
+
+  // useEffect(() => {
+  //   fetch(`http://scriptyper-backend:3010/api/git`)
+  //   .then(res => res.json())
+  //   .then(data => {
+  //     setArray(data);
+  //   }).catch(err => {
+  //     console.log(err)
+  //   });
+  // }, []);
+
+  const [intRandom, setIntRandom] = useState(0);
+  let chars: string = randomWordCss[intRandom]
+
+  // Call the hook
+  const [charsArray, setCharsArray] = useState(0);
+  const [charsState, setCharsState] = useState([0]);
+  const [calculatingScore, setCalculatingScore] = useState([0]);
+
+  const resetTyping = () => {
+    if(charsState.length === 1) {
+      let tmpCharsState = []
+      for(let n = 0; n < chars.split("").length; n++) {
+        tmpCharsState.push(0);
+      }
+      setCharsState(tmpCharsState);
+    }
+  }
+
+  resetTyping();
+
+  console.log(charsState);
+
+  const insertTyping = (keyName: string) => {
+    if(keyName === chars.split("")[charsArray]) {
+      charsState[charsArray] = 1;
+    } else {
+      charsState[charsArray] = 2;
+    }
+    setCharsArray(charsArray + 1);
+    setCharsState(charsState);
+  }
+
+  const deleteTyping = () => {
+    if(charsArray > 0) {
+      charsState[charsArray] = 0;
+      setCharsState(charsState);
+      setCharsArray(charsArray - 1);
+    }
+  }
+
+  const changeTyping = () => {
+    setCharsArray(0);
+    setCharsState([0]);
+    setIntRandom(intRandom + 1);
+    chars = randomWordCss[intRandom];
+    console.log(chars)
+    resetTyping();
+    console.log(charsState);
+  }
+
+  const judgeTyping = () => {
+    let tmpCalculatingScore = [...calculatingScore];
+    if(!charsState.includes(2)) {
+      tmpCalculatingScore.push(randomLevelCss[intRandom])
+      setCalculatingScore(tmpCalculatingScore);
+    } else {
+      tmpCalculatingScore.push(0);
+      setCalculatingScore(tmpCalculatingScore);
+    }
+  }
+
+  if(charsState[charsState.length - 1] === 1 || charsState[charsState.length - 1] === 2){
+    judgeTyping();
+    changeTyping();
+    console.log(calculatingScore);
+  }
+
+  // Capture and display!
+  if(calculatingScore.length === 10) {
+    for(let p = 0; p < calculatingScore.length; p++) {
+      result += calculatingScore[p];
+    }
+    console.log(result)
+    return (
+      <>
+      <h1>合計点： {result}</h1>
+      </>
+    )
+  } else {
+  return (
+    <>
+    <h1
+      onKeyDown={(e) => {
+        const key = e.key;
+        if (key === "Backspace") {
+          deleteTyping();
+        } else if (key.length === 1) {
+          insertTyping(key);
+        }
+        e.preventDefault()
+      }}
+      tabIndex={0}
+    >
+      {chars.split("").map((char, index) => {
+        let state = charsState[index];
+        return (
+          <span 
+          key={char + index} 
+          className={
+            state === 0 ? styles.char__default : 
+            state === 1 ? styles.char__true : 
+            styles.char__false
+          }
+          >
+            {char}
+          </span>
+        );
+      })}
+    </h1>
+    <p>LEVEL: {randomLevelCss[intRandom]}</p>
+    <p>{randomMeanCss[intRandom]}</p>
+    </>
+  );
+  }
+};
+export default TypingGameCss
+;
