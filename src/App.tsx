af20928fd4131794561ca1c00f2036abacc9edd2
AYATSUKO <cdrk1210470@gn.iwasaki.ac.jp>
2022-05-27 14:12:03 +0900

    リンクのスタイル変更＆トップに戻るボタンの有効化＆phpモードの追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import logo from "./logo.svg";
 import "./App.css";
 import TypingGameComponent from "./components/TypingGameComponent";
@@ -17,34 +17,37 @@ const App = () => {
     Javascript: WordContainment.js,
     Git: WordContainment.git,
     //ここの行から新たなモードを追加
-    Reset: WordContainment.reset
+    PHP: WordContainment.php
   };
 
   const modeSelectKeys = Object.keys(modeSelect),
-  modeSelectValue = Object.values(modeSelect);
+    modeSelectValue = Object.values(modeSelect);
 
-  const [mode, setMode] = useState(modeSelect.Reset);
-  const [modeScript, setModeScript] = useState('reset');
+  const [mode, setMode] = useState(modeSelect.HTML);
+  const [modeScript, setModeScript] = useState("HTML");
 
   const randomInt = TypingWordObject(mode);
 
-  const onClick = (selected: {
-    word: string;
-    mean: string;
-    level: number;
-  }[], path: string) => {
+  const onClick = (
+    selected: {
+      word: string;
+      mean: string;
+      level: number;
+    }[],
+    path: string
+  ) => {
     setMode(selected);
     setModeScript(path);
-  }
+  };
 
-    randomWordArr.length = 0;
-    randomMeanArr.length = 0;
-    randomLevelArr.length = 0;
-    for (let i = 0; i < 10; i++) {
-      randomWordArr.push(mode[randomInt[i]].word);
-      randomMeanArr.push(mode[randomInt[i]].mean);
-      randomLevelArr.push(mode[randomInt[i]].level);
-    }
+  randomWordArr.length = 0;
+  randomMeanArr.length = 0;
+  randomLevelArr.length = 0;
+  for (let i = 0; i < 10; i++) {
+    randomWordArr.push(mode[randomInt[i]].word);
+    randomMeanArr.push(mode[randomInt[i]].mean);
+    randomLevelArr.push(mode[randomInt[i]].level);
+  }
 
   const info = {
     randomWord: randomWordArr,
@@ -55,29 +58,54 @@ const App = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <p className="App-modenav-li">EXIT TO TOP</p>
-        <img src={logo} className="App-logo" alt="logo" />
         <BrowserRouter>
           <Routes>
-            <Route path={`/${modeScript}`} element={<TypingGameComponent randomWord={info.randomWord} randomMean={info.randomMean} randomLevel={info.randomLeVel} />} />
-            <Route path={'/'} element={
-              <>
-                <h3>モードを選択してください。</h3>
-                <ul className="App-modenav">
-                  {modeSelectKeys.map((value, index) => {
-                    return(
-                      <li key={index} className="App-modenav-li" onClick={() => {
-                        const selected = modeSelectValue[index], path = value;
-                        onClick(selected, path);
-                      }}>
-                        <Link to={`/${value}`}>{value}</Link>
-                      </li>
-                    )
-                  })}
-
-                </ul>
-              </>
-            } />
+            <Route
+              path={`/${modeScript}`}
+              element={
+                <Link to={"/"} className="App-modenav-li">
+                  ESCAPE TO TOP
+                </Link>
+              }
+            />
+          </Routes>
+          <img src={logo} className="App-logo" alt="logo" />
+          <Routes>
+            <Route
+              path={`/${modeScript}`}
+              element={
+                <TypingGameComponent
+                  randomWord={info.randomWord}
+                  randomMean={info.randomMean}
+                  randomLevel={info.randomLeVel}
+                />
+              }
+            />
+            <Route
+              path={"/"}
+              element={
+                <>
+                  <h3>モードを選択してください。</h3>
+                  <ul className="App-modenav">
+                    {modeSelectKeys.map((value, index) => {
+                      return (
+                        <li
+                          key={index}
+                          className="App-modenav-li"
+                          onClick={() => {
+                            const selected = modeSelectValue[index],
+                              path = value;
+                            onClick(selected, path);
+                          }}
+                        >
+                          <Link to={`/${value}`} className="App-modenav-li">{value}</Link>
+                        </li>
+                      );
+                    })}
+                  </ul>
+                </>
+              }
+            />
           </Routes>
         </BrowserRouter>
       </header>
