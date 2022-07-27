082d74f650a7781c561af7e33bd97218c58a580f
AYATSUKO <cdrk1210470@gn.iwasaki.ac.jp>
2022-07-27 10:16:45 +0900

    ワードのアニメーション撤廃＆タイトル変更

--- src/components/TypingGameComponent.tsx
+++ src/components/TypingGameComponent.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import TypingGameResult from "./TypingGameResult";
 import styles from "./TypingGameComponent.module.scss";
 
@@ -14,49 +14,48 @@ const TypingGameComponent: React.FC<TypingGameProps> = ({
   randomLevel,
 }) => {
   // Call the hook
-  const [classToggle, setClassToggle] = useState(false);
   const [intRandom, setIntRandom] = useState(0);
   const [charsArray, setCharsArray] = useState(0);
-  const [charsState, setCharsState] = useState([0]);
-  const [calculatingScore, setCalculatingScore] = useState([0]);
-
-  let chars: string[] = [randomWord[intRandom], randomWord[intRandom]];
-
+  const [charsStateL, setCharsStateL] = useState([0]);
+  const [charsStateR, setCharsStateR] = useState([0]);
+  const [calcScore, setCalcScore] = useState([0]);
+  let chars: string = randomWord[intRandom];
+  let isIntRandom: boolean = intRandom % 2 === 0;
+  let charsStateLR: number[] = isIntRandom ? charsStateL : charsStateR;
+  const isLastChar = (charsStateLR: number[]) => {
+    return (
+      (charsStateLR[charsStateLR.length - 1] === 1 ||
+        charsStateLR[charsStateLR.length - 1] === 2) &&
+      calcScore.length < randomLevel.length
+    );
+  };
   const changeCharsState = (charsStateArr: number[]) => {
-    setCharsState(charsStateArr);
-  }
-
+    if (isIntRandom) {
+      setCharsStateL(charsStateArr);
+    } else {
+      setCharsStateR(charsStateArr);
+    }
+  };
   const changeCharsArray = (charsNum: number) => {
     setCharsArray(charsNum);
-  }
-
+  };
   const changeIntRandom = (int: number) => {
     setIntRandom(int);
-  }
-
-  const changeCalculatingScore = (scoreArr: number[]) => {
-    setCalculatingScore(scoreArr);
-  }
-
-  const changeClassToggle = () => {
-    setClassToggle(!classToggle);
-  }
-
+  };
+  const changeCalcScore = (scoreArr: number[]) => {
+    setCalcScore(scoreArr);
+    console.log(calcScore); //レンダリング無限ループ中動かず
+  };
   const judgeTyping = () => {
-    if (
-      (charsState[charsState.length - 1] === 1 ||
-        charsState[charsState.length - 1] === 2) &&
-      calculatingScore.length < 10
-    ) {
-      calculatingScore[intRandom] = !charsState.includes(2)
-      ? randomLevel[intRandom]
-      : 0;
-    changeCalculatingScore(calculatingScore);
+    if (isLastChar(charsStateLR)) {
+      calcScore[intRandom] = !charsStateLR.includes(2)
+        ? randomLevel[intRandom]
+        : 0;
+      changeCalcScore(calcScore);
     }
-  }
-
+  };
   const resetTyping = () => {
-    if (calculatingScore.length !== 10 && charsState.length === 1) {
+    if (calcScore.length !== 10 && charsStateLR.length === 1) {
       let tmpCharsState = [];
       for (let n = 0; n < randomWord[intRandom].split("").length; n++) {
         tmpCharsState.push(0);
@@ -64,107 +63,80 @@ const TypingGameComponent: React.FC<TypingGameProps> = ({
       changeCharsState(tmpCharsState);
     }
   };
-
-  resetTyping();
-
-  console.log(charsState);
-  console.log(classToggle);
-
   const insertTyping = (keyName: string) => {
     if (keyName === randomWord[intRandom].split("")[charsArray]) {
-      charsState[charsArray] = 1;
+      charsStateLR[charsArray] = 1;
     } else {
-      charsState[charsArray] = 2;
+      charsStateLR[charsArray] = 2;
     }
     changeCharsArray(charsArray + 1);
-    changeCharsState(charsState);
+    changeCharsState(charsStateLR);
   };
-
   const deleteTyping = () => {
     if (charsArray > 0) {
-      charsState[charsArray] = 0;
+      charsStateLR[charsArray] = 0;
       changeCharsArray(charsArray - 1);
-      changeCharsState(charsState);
+      changeCharsState(charsStateLR);
     }
   };
-
   const changeTyping = () => {
-    if (
-      (charsState[charsState.length - 1] === 1 ||
-        charsState[charsState.length - 1] === 2) &&
-      calculatingScore.length < 10
-    ) {
-      changeClassToggle();
+    if (isLastChar(charsStateLR)) {
+      changeIntRandom(intRandom + 1);
       changeCharsArray(0);
       changeCharsState([0]);
-      changeIntRandom(intRandom + 1);
-      if (chars[0] === randomWord[intRandom - 1] || chars[1] === null) {
-        chars[0] = randomWord[intRandom];
-      } else if (chars[1] === randomWord[intRandom - 1] || chars[1] === null) {
-        chars[1] = randomWord[intRandom];
-      }
       resetTyping();
     }
   };
-
   judgeTyping();
   changeTyping();
-  //console.log(calculatingScore);
+  resetTyping();
+  console.log(charsStateL);
 
   // Capture and display!
   if (
-    calculatingScore.length === 10 &&
-    (charsState[charsState.length - 1] === 1 ||
-      charsState[charsState.length - 1] === 2)
+    calcScore.length === randomLevel.length &&
+    (charsStateLR[charsStateLR.length - 1] === 1 ||
+      charsStateLR[charsStateLR.length - 1] === 2)
   ) {
     return (
       <TypingGameResult
-        calculatingScore={calculatingScore}
+        calculatingScore={calcScore}
         randomLevel={randomLevel}
       />
     );
   } else {
     return (
-      <div>
-        <div className={classToggle === true ? styles.swiper__right : styles.swiper__left}>
-          {chars.map((value, index) => {
+      <div className={styles.TypingGameComponent}>
+        <h1
+          onKeyDown={(e) => {
+            const key = e.key;
+            if (key === "Backspace") {
+              deleteTyping();
+            } else if (key.length === 1) {
+              insertTyping(key);
+            }
+            e.preventDefault();
+          }}
+          tabIndex={0}
+        >
+          {chars.split("").map((char, index) => {
+            let state = charsStateLR[index];
             return (
-              <div key={index} className={styles.words}>
-                <h1
-                  //className={classToggle === 2 || classToggle === 0 ? styles.string__in : classToggle === 1 ? styles.string__out : undefined}
-                  onKeyDown={(e) => {
-                    const key = e.key;
-                    if (key === "Backspace") {
-                      deleteTyping();
-                    } else if (key.length === 1) {
-                      insertTyping(key);
-                    }
-                    e.preventDefault();
-                  }}
-                  tabIndex={0}
-                >
-                  {value.split("").map((char, index) => {
-                    let state = charsState[index];
-                    return (
-                      <span
-                        key={char + index}
-                        className={
-                          state === 0
-                            ? styles.char__default
-                            : state === 1
-                              ? styles.char__true
-                              : styles.char__false
-                        }
-                      >
-                        {char}
-                      </span>
-                    );
-                  })}
-                </h1>
-              </div>
+              <span
+                key={char + index}
+                className={
+                  state === 0
+                    ? styles.char__default
+                    : state === 1
+                    ? styles.char__true
+                    : styles.char__false
+                }
+              >
+                {char}
+              </span>
             );
           })}
-        </div>
+        </h1>
         <p className={styles.words__option}>LEVEL: {randomLevel[intRandom]}</p>
         <p className={styles.words__option}>{randomMean[intRandom]}</p>
       </div>
